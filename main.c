f42f9ac4c2addfd9c741fdbab280a5ec05b16d6e
Melina <mrequena@fue.undef.edu.ar>
2023-04-19 14:05:15 -0300

    Ej 8

--- main.c
+++ main.c
@@ -1,6 +1,21 @@
-#include <stdio.h>
+/* Guia 0,  Ej 7
+ Escriba un programa que declare una variable entera y que compruebe si es par o
+impar utilizando el operador módulo (%). */
 
-int main() {
-    printf("Hello, World!\n");
+#include<stdio.h>
+void paroimpar(int *);
+int main(){
+    int num, *numptr;
+    printf("Ingrese el valor del num:\n");
+    scanf("%d", &num);
+    numptr = &num;
+    paroimpar(numptr);
     return 0;
+}
+
+void paroimpar(int *numptr){
+    if((*numptr)%2== 0){
+        printf("El numero ingresado es par\n");
+    } else
+        printf("El numero es impar\n");
 }
